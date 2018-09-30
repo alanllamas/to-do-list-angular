eeb5621633726fcee4d04c0fbf3e2977119185cd
alanllamas <alan@alans-MacBook-Pro.local>
2018-09-30 02:16:58 -0500

    added markup ready to recieve functionality

---
+++ to-do/src/app/task-done-list/task-done-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'task-done-list',
+  templateUrl: './task-done-list.component.html',
+  styleUrls: ['./task-done-list.component.scss']
+})
+export class TaskDoneListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
